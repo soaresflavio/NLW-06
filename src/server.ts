5692428fbb5a08e93838ac84edc09833c5417b18
root <root@pop-os.localdomain>
2021-06-22 14:27:13 -0300

    BD

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,8 @@
+import "reflect-metadata";
 import express from "express";
 
+import "./database";
+
 const app = express();
-
-app.get("/test", (request, response) => {
-    
-    return response.send("Ola NLW 06");  
-
-});
-
-app.post("/test-post", (request, response) => {
-
-    return response.send("Ola NLW metodo POST");
-
-});
 
 app.listen(3000, () => console.log("Server is running"));

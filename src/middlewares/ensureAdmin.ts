ab41abc1f32dc9b9ed312b962df287dd5afc5cbc
root <root@pop-os.localdomain>
2021-06-26 13:14:06 -0300

    authenticate

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,8 +1,16 @@
-import { Request, Response, NextFunction } from "express"
+import { Request, Response, NextFunction } from "express";
+import { UsersRepositories } from "../repositories/UsersRepositories";
+import { getCustomRepository } from "typeorm";
 
 
-export function ensureAdmin(request: Request, response: Response, next: NextFunction) {
-    const admin = true;
+export async function ensureAdmin(request: Request, response: Response, next: NextFunction) {
+    
+    const { user_id } = request;
+    console.log(user_id);
+
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    const { admin } = await usersRepositories.findOne(user_id);
 
     if (admin) {
         return next();
